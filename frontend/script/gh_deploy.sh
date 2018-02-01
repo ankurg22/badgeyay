0b631541492bd5edb3189af52b619125ce698852
Ankur Gupta <guptaankur.gupta0@gmail.com>
2018-02-02 01:32:59 +0530

    Update gh_deploy.sh

--- frontend/script/gh_deploy.sh
+++ frontend/script/gh_deploy.sh
@@ -3,7 +3,7 @@
 git config --global user.name "Travis CI"
 git config --global user.email "noreply+travis@fossasia.org"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "ankurg22/badgeyay"]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "ankurg22/badgeyay" ]; then
     echo "Skipping deploy; The request or commit is not on development:)"
     exit 0
 fi
