001ce70fa6f98797937b33c5fdcf5ef2449d9732
Ankur Gupta <guptaankur.gupta0@gmail.com>
2018-02-02 00:51:30 +0530

    Update gh_deploy.sh

--- frontend/script/gh_deploy.sh
+++ frontend/script/gh_deploy.sh
@@ -6,7 +6,7 @@ git config --global user.email "noreply+travis@fossasia.org"
 
 export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/badgeyay" -o "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "ankurg22/badgeyay" -o "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "Skipping deploy; The request or commit is not on development:)"
     exit 0
 fi
